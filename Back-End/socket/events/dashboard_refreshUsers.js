eabced0732237318da3512ec5584bb183548d9e3
bekX0 <hikmetberkin@gmail.com>
2023-08-01 15:52:44 +0300

    changed socket response

--- Back-End/socket/events/dashboard_refreshUsers.js
+++ Back-End/socket/events/dashboard_refreshUsers.js
@@ -3,14 +3,6 @@ const { pool, Pool } = require("../../utils/connection.js")
 module.exports = {
     name:"dashboard/refreshUsers",
     execute: (ws, data) => {
-        return new Promise((resolve, reject) => {
-            pool.query('SELECT * FROM users', [], (err, results) => {
-                if(err){
-                    console.log(err);
-                    reject(err);
-                }
-                resolve(JSON.stringify({name:"refresh/usersTable", data: results.rows}));
-            })
-        })
+        return JSON.stringify({name:"refresh/usersTable"})
     }
 }
