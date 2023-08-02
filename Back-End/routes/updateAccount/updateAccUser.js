4b9f8cff22e4e3fc846ee69661ba433939498ee5
damildot <damlatatar06@gmail.com>
2023-08-02 13:42:00 +0300

    add files
    added some files

---
+++ Back-End/routes/updateAccount/updateAccUser.js
@@ -0,0 +1,16 @@
+const { pool, Pool } = require("../../utils/connection.js");
+
+module.exports = {
+  name: "user/updateAccount",
+  execute: async (req, res) => {
+    let email = req.headers.auth.split(" ")[0];
+    let { password } = req.body;
+    try {
+      const query = "UPDATE accounts SET password=$1 WHERE email=$2";
+      await pool.query(query, [password, email]);
+      res.json({ message: "HERE" });
+    } catch (err) {
+      res.status(500).json({ error: "there is a error" });
+    }
+  },
+};
